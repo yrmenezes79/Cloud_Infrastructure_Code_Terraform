87a94525f2fdd8f483fbe34cc2dc11bd611ab22d
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-10-04 11:15:20 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
