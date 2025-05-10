b4d707c6c2c0a0a8ac9459bf60da5334ff176d3b
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-05-10 11:37:21 -0300

    Update install_ec2.tf

--- projeto1/install_ec2.tf
+++ projeto1/install_ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Terraform" {
   count                  = var.count_instance
   ami                    = var.aws_ami
   instance_type          = var.aws_instance_type
-  security_groups        = ["sg_DefaultWebserver"]
+  security_groups        = var.securtity_aws
   key_name               = var.aws_key
   user_data              = file("install.sh")
 
