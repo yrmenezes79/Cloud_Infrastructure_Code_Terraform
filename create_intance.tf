43d0dec324e6beb78a5fb375e302dd38d4e48d82
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-09-01 22:08:36 -0300

    Update create_intance.tf

--- create_intance.tf
+++ create_intance.tf
@@ -1,7 +1,7 @@
 # Recurso para criar uma instância EC2
 resource "aws_instance" "example" {
-  ami           = "ami-0866a3c8686eaeeba"  # AMI do Ubuntu 20.04 (exemplo, pode variar conforme a região)
-  instance_type = "t2.micro"               # Tipo de instância (elegível para Free Tier)
+  ami           = "ami-0360c520857e3138f"  # AMI do Ubuntu 20.04 (exemplo, pode variar conforme a região)
+  instance_type = "t3.micro"               # Tipo de instância (elegível para Free Tier)
 }
 
 # Output para mostrar o IP público da instância
