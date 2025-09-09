a339a8b2dd21eae2a0677789e3abce0cd0a012db
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-09-09 09:52:42 -0300

    Update install_ec2.tf

--- projeto1/install_ec2.tf
+++ projeto1/install_ec2.tf
@@ -1,13 +1,13 @@
-#Criando uma instancia EC2
 resource "aws_instance" "Terraform" {
-  count                  = var.count_instance
-  ami                    = var.aws_ami
-  instance_type          = var.aws_instance_type
-  security_groups        = var.securtity_aws
-  key_name               = var.aws_key
-  user_data              = file("install.sh")
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  # Ajuste feito aqui: precisa ser uma lista, não string
+  security_groups = var.securtity_aws
+
+  user_data = file("install.sh")
 
   tags = {
-    Name = "ec2-instance-terraform-tech${count.index + 1}"
+    Name = "EC2-Terraform"
   }
 }
