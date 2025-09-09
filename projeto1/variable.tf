f7c1c6cb977e7c198bd40d7c43a17a70ddb3de13
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-09-09 09:54:01 -0300

    Update variable.tf

--- projeto1/variable.tf
+++ projeto1/variable.tf
@@ -1,25 +1,31 @@
-variable "count_instance" {
-  description = "Quantidade de instancias"
-  type        = number
-  default     = 2
-}
-
-variable "aws_ami" {
-  description = "aws ami default"
+variable "aws_region" {
+  description = "Região AWS"
   type        = string
-  default     = "ami-0360c520857e3138f" #Alterar a imagem
+  default     = "us-east-1"
 }
 
-variable "aws_instance_type" {
-  description = "aws instance default"
+variable "aws_profile" {
+  description = "Perfil de credenciais AWS"
+  type        = string
+  default     = "default"
+}
+
+variable "ami_id" {
+  description = "AMI utilizada para criar a instância"
+  type        = string
+  default     = "ami-0360c520857e3138f" 
+}
+
+variable "instance_type" {
+  description = "Tipo da instância EC2"
   type        = string
   default     = "t2.micro"
 }
 
-variable "aws_region" {
-  description = "aws region default"
-  type        = string
-  default     = "us-east-1"
+variable "securtity_aws" {
+  description = "Lista de security groups a serem associados"
+  type        = list(string)
+  default     = ["allow_ssh_http"] 
 }
 
 variable "aws_key" {
@@ -27,8 +33,4 @@ variable "aws_key" {
   type        = string
   default     = "ansible1" #Alterar a chave
 }
-variable "securtity_aws" {
-  description = "aws SG default"
-  type        = string
-  default     = "sg_DefaultWebserver"
-}
+
