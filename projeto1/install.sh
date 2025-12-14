d58a09830dfe12ff4d3ede2ea65c7ed261ac550d
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-12-14 10:36:59 -0300

    Update install.sh

--- projeto1/install.sh
+++ projeto1/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y docker.io
+# Atualiza os pacotes
+sudo dnf update -y
+# Instala o Docker
+sudo dnf install -y docker
+# Inicia e habilita o serviço Docker
 sudo systemctl start docker
 sudo systemctl enable docker
-# Detecta o usuário padrão (Ubuntu ou Amazon Linux)
+# Detecta o usuário padrão (UID 1000)
 DEFAULT_USER=$(getent passwd 1000 | cut -d: -f1)
+# Adiciona o usuário ao grupo docker
 sudo usermod -aG docker $DEFAULT_USER
+
+
